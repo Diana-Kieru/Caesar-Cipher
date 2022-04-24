85507d592decd60914f59c313ca3e615bedfa96a
diana <dianakieru1@gmil.com>
2022-04-25 01:21:03 +0300

    made correction on where I was writting my test

--- src/test/java/DecryptTest.java
+++ src/test/java/DecryptTest.java
@@ -1,2 +1,13 @@
+import Ceaser.Decrypt;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class DecryptTest {
+    @Test
+    public  void  decrypt_notNull_Exception(){
+        Decrypt decrypt = new Decrypt("ABCD",3);
+        assertNotNull(decrypt);
+    }
+
 }
--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -1,12 +1,5 @@
-import Ceaser.Decrypt;
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class EncryptTest {
-    @Test
-    public  void  decrypt_notNull_Exception(){
-        Decrypt decrypt = new Decrypt("ABCD",3);
-        assertNotNull(decrypt);
-    }
+
 }
