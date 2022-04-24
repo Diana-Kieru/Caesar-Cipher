e8dbf6cb6f1472135db2dd5c2d3ffb7eb4ed2dfe
diana <dianakieru1@gmil.com>
2022-04-25 01:13:34 +0300

    added encryption key

--- src/main/java/Ceaser/Encrypt.java
+++ src/main/java/Ceaser/Encrypt.java
@@ -1,4 +1,11 @@
 package Ceaser;
 
+
 public class Encrypt {
-}
+
+    private String encrypted = "";
+    public String encoding(String plainText, int key) throws Exception {
+        int n = plainText.length();
+
+        if (key < 1 || key > 25) throw new Exception("The key must be between 1 and 25");
+
