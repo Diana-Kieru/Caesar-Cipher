4d3b5aab1f5102e55afa94e1f4543d1f53787ef0
diana <dianakieru1@gmil.com>
2022-04-25 01:23:32 +0300

    added string encryption test

--- src/test/java/DecryptTest.java
+++ src/test/java/DecryptTest.java
@@ -2,12 +2,19 @@ import Ceaser.Decrypt;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DecryptTest {
     @Test
     public  void  decrypt_notNull_Exception(){
         Decrypt decrypt = new Decrypt("ABCD",3);
         assertNotNull(decrypt);
+    }
+    @Test
+    public  void  decrypt_onlyString_Exception(){
+        Decrypt decrypt = new Decrypt("BC",1);
+        decrypt.getDecrypted();
+        assertTrue(decrypt.getDecrypted() instanceof String );
     }
 
 }
