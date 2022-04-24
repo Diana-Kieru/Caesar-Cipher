17d7805217e8751b37cb26ebddd5e6f91668500a
diana <dianakieru1@gmil.com>
2022-04-25 01:37:40 +0300

    declared variables

--- src/main/java/Ceaser/Decrypt.java
+++ src/main/java/Ceaser/Decrypt.java
@@ -1,4 +1,12 @@
 package Ceaser;
 
 public class Decrypt {
+    private String decrypted="";
+    private String encrypted;
+    private int key;
+
+    public Decrypt(String encrypted, int key){
+        this.encrypted = encrypted;
+        this.key = key;
+    }
 }
