5bd3f8f14ea16761aa7d7b53d62485af31c02d53
diana <dianakieru1@gmil.com>
2022-04-25 03:16:55 +0300

    added encryption Test

--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -1,5 +1,27 @@
+import Ceaser.Encrypt;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EncryptTest {
+    @Test
+    public  void encrypt_onlyString_exception() throws Exception {
+        //arrange
+        Encrypt encrypt = new Encrypt();
+
+        //act
+        String actual = encrypt.encoding("1",1);
+
+        //assert
+        assertTrue(actual instanceof String);
+
+    }
+    @Test
+    public  void  encrypt_notNull_Exception() throws Exception {
+        Encrypt encrypt = new Encrypt();
+        String  actual = encrypt.encoding("AB",1);
+        assertNotNull(actual);
+    }
 
 }
