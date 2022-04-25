39fcd7a2fa35b0239591ef74997cb931fbf586bf
diana <dianakieru1@gmil.com>
2022-04-25 03:29:33 +0300

    added an array to loop through char indexes

--- src/main/java/Ceaser/Decrypt.java
+++ src/main/java/Ceaser/Decrypt.java
@@ -12,4 +12,14 @@ public class Decrypt {
     public void Decode() throws Exception {
         if(key < 1 || key > 25) throw new Exception("The key must be between 1 and 25");
         int n = encrypted.length();
+        for (int i=0;i<n;i++){
+            char letter = encrypted.charAt(i);
+            if(Character.isLetter(letter)){
+                if (Character.isUpperCase(letter)){
+                    char r = (char)(letter - key);
+                    if(r > 'Z'){
+                        decrypted += (char)(letter-(26+key));
+                    }else {
+                        decrypted += r;
+                    }
 }
